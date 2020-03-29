11042fb839812586c9b9eb9e54fdfd8bb6673ae5
amin <aminasa72@gmail.com>
2020-03-29 19:57:46 +0000

    add rest_framework and rest_framework.authtoken to installed apps

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+
     # local apps
     'profiles_api.apps.ProfilesApiConfig',
 ]
